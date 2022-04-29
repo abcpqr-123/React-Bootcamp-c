74917b4c7a858cacdd18406df9d89de80d2d37bb
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-04-29 21:55:25 +0530

    NumPicker

---
+++ 004_App_Structure/NumPicker.js
@@ -0,0 +1,30 @@
+function getNum() {
+    return Math.floor(Math.random() * 10) + 1
+}
+
+
+class NumPicker extends React.Component {
+render(){
+  const num = getNum()
+  let msg
+  if (num === 7){
+     
+      msg =
+      <div>
+          <h2>Congrats</h2>
+          <h3>Great!</h3>
+      </div>
+  }else{
+      msg = <p>Sorry! u lose!</p>
+  }
+  return (
+
+      <div>
+      <h1> Your number is: {num}</h1>
+     {msg}
+      </div> 
+  )
+  
+}
+}
+
