c38e5bf53ffd0ed7ab57bede25b09f936165b870
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-08 01:49:31 +0530

    helpers.js

---
+++ 010_our_first-app/src/helpers.js
@@ -0,0 +1,15 @@
+function helpful(){
+    console.log('I did a super helpful thing!')
+}
+
+function sort(){
+    console.log('ALL SORTED!')
+}
+
+function sing(){
+    console.log('LA LA LA')
+}
+
+export default helpful;
+
+export { sort, sing}
