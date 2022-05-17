bde556f3542e9031aafb48735082c4a1694c919b
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-17 18:08:40 +0530

    App.js

---
+++ 016_Colors_Exercise_Solution/colors_exercise_solution/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import boxes from './boxes';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <boxes />
+      </div>
+    )
+  }
+}
+
+export default App;
+
+// import React, { Component } from 'react'
+// import boxes from './boxes'
+// import './App.css';
+
+// class App extends Component {
+//   render(){
+//     return(
+//       <div>
+//         <boxes />
+//       </div>
+//     )
+//   }
+// }
+
+
+
+// export default App;
+
+
+
+
+// import './App.css';
+// import boxes from './boxes.js'
+// function App() {
+//   return (
+//     <div className="App">
+//       </ boxes.js>
+//     </div>
+    
+//   );
+// }
+
+// export default App;
