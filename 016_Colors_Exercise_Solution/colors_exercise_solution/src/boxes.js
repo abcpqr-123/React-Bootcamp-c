6d802a021fb829fa34f05c1a224d5f0bc1933271
abcpqr-123 <surya.gaonkar.signup@gmail.com>
2022-05-17 17:53:31 +0530

    boxes.js

---
+++ 016_Colors_Exercise_Solution/colors_exercise_solution/src/boxes.js
@@ -0,0 +1,17 @@
+
+
+import React, { Component } from 'react'
+import './boxes.css'
+
+    class boxes extends Component{
+    render() {
+            return (
+            <div className='boxes'>
+              <h1>hi</h1>
+            </div>
+           )
+            }
+    }
+
+
+export default boxes
